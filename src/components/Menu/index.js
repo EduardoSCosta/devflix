68eeeca86cd85fee735838a3c1e56482ab5926e9
EduardoSCosta <edu_costaaraujo@hotmail.com>
2020-07-28 17:52:45 -0300

    Navbar Created

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './menu.css';
+// import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+function Menu(){
+  return(
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Devflix Logo"/>
+      </a>
+      <Button as="a" className="ButtonLink" href="/">
+        Novo video
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
