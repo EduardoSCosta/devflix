a15dbcf2747535a996d17f110053a6d0d267cef9
EduardoSCosta <edu_costaaraujo@hotmail.com>
2020-07-29 18:59:51 -0300

    Cadastro de Categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import './menu.css';
 // import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
+
+
 function Menu(){
   return(
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="Devflix Logo"/>
-      </a>
-      <Button as="a" className="ButtonLink" href="/">
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo video
       </Button>
     </nav>
